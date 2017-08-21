0f41ce9f021cead6e80b17427e38eaa016290deb
Michael Robbins <michael@dalmura.com.au>
2017-08-21 22:58:14 +1000

    Clean up the testing run

--- test_data/run_output_comparison.sh
+++ test_data/run_output_comparison.sh
@@ -3,26 +3,30 @@
 BIN='../target/debug/file_mapper'
 CONF='test_config.yaml'
 
-OUTPUT='/tmp/output.tsv'
-EXPECTED_OUTPUT='expected_output.tsv'
+ACTUAL_OUTPUT='/tmp/output.tsv'
+EXPECT_OUTPUT='expected_output.tsv'
 
+# Compile any changes and perform a mapping run
 cargo build
 $BIN --config-file $CONF > /tmp/output.tsv
 
-OUTPUT_HASH=`md5sum ${OUTPUT} | awk '{print $1}'`
-EXPECTED_OUTPUT_HASH=`md5sum ${EXPECTED_OUTPUT} | awk '{print $1}'`
+# Generate the hashes
+ACTUAL_HASH=`md5sum ${ACTUAL_OUTPUT} | awk '{print $1}'`
+EXPECT_HASH=`md5sum ${EXPECT_OUTPUT} | awk '{print $1}'`
 
-echo "NOTICE: Output Hash: ${OUTPUT_HASH}"
-echo "NOTICE: Expected Output Hash: ${EXPECTED_OUTPUT_HASH}"
+echo "NOTICE: Actual Hash: ${ACTUAL_HASH}"
+echo "NOTICE: Expect Hash: ${EXPECT_HASH}"
 
-if [ "${OUTPUT_HASH}" != "${EXPECTED_OUTPUT_HASH}" ]; then
+# Report on outcome
+if [ "${ACTUAL_HASH}" != "${EXPECT_HASH}" ]; then
     echo "ERROR: Hashes were different!"
-    echo ${OUTPUT}
-    echo ${EXPECTED_OUTPUT}
-    diff ${OUTPUT} ${EXPECTED_OUTPUT}
+    echo ${ACTUAL_OUTPUT}
+    echo ${EXPECT_OUTPUT}
+    diff ${ACTUAL_OUTPUT} ${EXPECT_OUTPUT}
 else
     echo "NOTICE: Hashes match for output:"
-    cat ${OUTPUT}
+    cat ${ACTUAL_OUTPUT}
 fi
 
-rm ${OUTPUT}
+rm ${ACTUAL_OUTPUT}
+
